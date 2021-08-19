b1bc99a5f65c9901f9952281914ce3aeb95610cb
KARTHIKEYAN GANESH <37135347+kn123g@users.noreply.github.com>
2021-08-19 16:25:20 +0530

    Create temp.js

---
+++ temp.js
@@ -0,0 +1,30 @@
+// Getting input via STDIN
+const readline = require("readline");
+
+const inp = readline.createInterface({
+  input: process.stdin
+});
+
+const userInput = [];
+
+inp.on("line", (data) => {
+  userInput.push(data);
+});
+
+inp.on("close", () => {
+  //start-here
+  //Your code goes here … replace the below line with your code logic 
+    let words = userInput[1].split(' '),n = +userInput[0];
+    for(let i=0;i<n; i++){
+        for(let j=i;j<n; j++){
+            if(words[i].localeCompare(words[j]) > 0){
+                let temp = words[i];
+                words[i] = words[j];
+                words[j] = temp;
+            } 
+        }
+    }
+    console.log(words.join(' '))
+
+  //end-here
+});
